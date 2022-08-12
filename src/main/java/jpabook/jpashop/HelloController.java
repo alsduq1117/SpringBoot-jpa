53f8f4991cf43b858759bde7e35a665b86695d7a
Minyeob Kim <alsduq1117@naver.com>
2022-08-13 03:38:28 +0900

    프로젝트 환경설정 commit

---
+++ src/main/java/jpabook/jpashop/HelloController.java
@@ -0,0 +1,17 @@
+package jpabook.jpashop;
+
+import lombok.Getter;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+
+    @GetMapping("hello")
+    public String Hello(Model model){
+        model.addAttribute("data","helllo!!");
+        return "hello";
+    }
+}
